671fb0ec8539ce0ac32872845badd9491863d2ca
nahomHmichael <nahomhabtemichael@gmail.com>
2022-09-06 21:16:20 +0300

    data_clean: Update

--- scripts/data_clean.py
+++ scripts/data_clean.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import numpy as np
-from log_help import App_Logger 
-app_logger = App_Logger('../logs/data_clean.log').get_app_logger()
+from log_helper import logger
 
 class DataClean:
     def __init__(self,df,deep=False) -> None:
-        self.logger = App_Logger(
-            '../logs/data_clean.log').get_app_logger()
         if(deep):
             self.df = df.copy(deep=True)
         else:
             self.df = df
+        logger.info("DataClean method added!")
             
     def remove_unwnated_columns(self,cols):
         self.df.drop(cols,axis=1,inplace=True)
+        logger.info("removing unwanted columns successful!")
         return self.df
     
     def remove_nulls(self) -> pd.DataFrame:
+        logger.info("removing nulls successful!")
         return self.df.dropna()
-    
+       
     def remove_duplicates(self):
         rmv = self.df[self.df.duplicated()].index
+        logger.info("removing duplicates successful!")
         return self.df.drop(index=rmv, inplace=True)
 
-    def fix_outlier(Self,cols):
+    def fix_outlier(self,cols):
         for col in cols:
             self.df[col] = np.where(self.df[col]>self.df[col].quantile(0.95),self.df[col].median(),self.df[col])
+        logger.info("removing outliers successful!")
