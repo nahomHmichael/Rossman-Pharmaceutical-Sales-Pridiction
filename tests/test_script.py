a97c7eb8d4989ed5e6aa109efe7cdef37ed849d1
nahomHmichael <nahomhabtemichael@gmail.com>
2022-09-06 10:15:44 +0300

    Finishing structuring

---
+++ tests/test_script.py
@@ -0,0 +1,11 @@
+import unittest
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..')))
+
+
+
+class TestCases(unittest.TestCase):
+      pass
+
+if __name__ == '__main__':
+    unittest.main()
