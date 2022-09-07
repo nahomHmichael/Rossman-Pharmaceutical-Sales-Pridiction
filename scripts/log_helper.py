5e6c4acc643176aaccd0dfd5845e432704b383dc
nahomHmichael <nahomhabtemichael@gmail.com>
2022-09-07 10:01:09 +0300

    script:Modified log_helper

--- scripts/log_helper.py
+++ scripts/log_helper.py
@@ -1,11 +1,26 @@
 from asyncio.log import logger
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('../logs/data_cleaning.log')
-formatter = logging.Formatter("time: %(asctime)s, function: %(funcName)s, module: %(name)s, message: %(message)s \n")
+#logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+#file_handler = logging.FileHandler('../logs/data_cleaning.log')
+#formatter = logging.Formatter("time: %(asctime)s, function: %(funcName)s, module: %(name)s, message: %(message)s \n")
 
-file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.INFO)
-logger.addHandler(file_handler)
+#file_handler.setFormatter(formatter)
+#file_handler.setLevel(logging.INFO)
+#logger.addHandler(file_handler)
+
+class Logger_class: 
+    def __init__(self,name,level=logging.INFO) :
+        logger = logging.getLogger(__name__)
+        logger.setLevel(level)
+        file_handler = logging.FileHandler(name)
+        formatter = logging.Formatter("time: %(asctime)s, \
+                                      function: %(funcName)s, module: %(name)s, message: %(message)s \n")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(name)
+        self.logger = logger
+    def get_logger(self):
+        return self.logger
+    
+        
